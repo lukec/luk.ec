beed50324f502aa2e3467a3f30da3fa0c38004e7
Luke Closs <luke@5thplane.com>
2013-07-14 09:09:12 -0700

    install vim too

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Hello lukec, good to see you.  Setting up your machine...";
 
-sudo apt-get install screen
+sudo apt-get install screen vim
 curl -q luk.ec/env/screenrc >> ~/.screenrc
 curl -q luk.ec/env/vimrc >> ~/.vimrc
