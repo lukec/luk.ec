dfcc91676619abf188cb64748dd706e143e71435
Luke Closs <me@luk.ec>
2016-05-24 23:22:45 -0700

    improve the setup scirpt

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-echo "Hello lukec, good to see you.  Setting up your machine...";
+#
+# To use this script: curl -q luk.ec/setup.sh
+#
 
 sudo apt-get install screen vim
-curl -q luk.ec/env/screenrc >> ~/.screenrc
-curl -q luk.ec/env/vimrc >> ~/.vimrc
+if [[ ! -e ~/.screenrc ]]; then
+    curl -q luk.ec/env/screenrc >> ~/.screenrc
+fi
+
+if [[ ! -e ~/.vimrc ]]; then
+    curl -q luk.ec/env/vimrc >> ~/.vimrc
+fi
+
+if [[ ! -e ~/.bashrc ]]; then
+    curl -q luk.ec/env/bashrc >> ~/.bashrc
+fi
+
+if [[ ! -e ~/.aliases ]]; then
+    curl -q luk.ec/env/aliases >> ~/.aliases
+fi
